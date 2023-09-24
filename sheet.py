9151bea162b5fb2a912619da5096ee10713b3e98
Cleiflym <cleiflym@gmail.com>
2023-09-23 21:58:10 -0300

    Pequenos upgrades e correções, agregado de manutenção de código.

--- sheet.py
+++ sheet.py
@@ -7,11 +7,22 @@ print("Bem-vindo ao sistema!")
 
 
 
+# Insert Data
+
+def add_column(df):
+
+    print("")
+
+    name_column = input("Nome da coluna: ")
+    print("Inseria os dados na ordem das colunas. Caso contrário, os dados saíram vazios.")
+ 
+def add_line(df):
+    # Pega a lista de resposta e insere na linha do DataFrame e returna um dataframe com a nova linha 
+    df.loc[len(df.index)+1] = questions(list(df.columns))
+    return df
 
 
-
-
-
+# Questions for insert the data
 def questions(list_questions):
 
     # Função que recebe uma lista de perguntas e retorna uma lista de respostas
@@ -22,15 +33,7 @@ def questions(list_questions):
 
     return response
 
-def add_line(df):
-    
-    # Pega a lista de resposta e insere na linha do DataFrame e returna um dataframe com a nova linha 
-    df.loc[len(df.index)+1] = questions(list(df.columns))
-    return df
-
-
-def define():
-    
+def define():    
     try:
 
         df = pd.read_csv(input("Digite o arquivo e sua localização: "), sep=input("Digite o separador: "))
@@ -44,35 +47,63 @@ def define():
         print("Um erro ocorreu. Tente novamente.")
         return False
 
-def show(df):
-    df.display()
+
+
+def cshow(df):
+    print(df.columns)
+    print(df[input("colunm: ").split(",")])
+
+def lshow(df):
+    print(df.index)
+    print(df.loc[[int(x) for x in input("line: ").split(",") ]])
+
+
+
+def options():
+    print("* imp - definir dataframe")
+    print("* exp - exportar dataframe para csv")
+    print("* iline - inserir linha no dataframe")
+    # print("* icol - inserir coluna")
+    print("* r(colum, line) - mudar um dado em uma determinada coluna e linha")
+    print("* show - exibir dataframe")
+    print("     * cshow(colum) - exibir determinada coluna")
+    print("     * lshow(index) - exibir determinada linha do dataframe ")
+    print("* exit - sair do sistema")
+
+
 
 def menu():
     system = True
     var = "NOT"
     df = ""
-    options()
+
     while(system):
         var = input(">> ")
-        if(var == "imp"):
-            df = define()
-        elif(var == "insert"):
-            df = add_line(df)
-        elif(var == "show"):
-            print(df)
-        elif(var == "exp"):
-            df.to_csv(input("Nome do documento: "), index=False, sep=input("Separador: "))
-        elif(var == "exit"):
-            system=False
-            print("Adeus...")
-        else:
-            print("Digite novamente")
+        try:
+            if(var == "imp"):
+                df = define()
+            elif(var == "iline"):
+                df = add_line(df)
+            # elif(var == "icol"):
+            #     df = add_column(df)
+            elif(var == "show"):
+                print(df)
+            elif(var == "cshow"):
+                cshow(df)
+            elif(var == "lshow"):
+                lshow(df)
+            elif(var == "options"):
+                options()
+            elif(var == "exp"):
+                df.to_csv(input("Nome do documento: "), index=False, sep=input("Separador: "))
+            elif(var == "exit"):
+                system=False
+                print("Adeus...")
+            else:
+                print("Digite novamente")
+                options()
 
-def options():
-    print("imp - definir dataframe")
-    print("insert - inserir linha no dataframe")
-    print("show - exibir dataframe")
-    print("exp - exportar dataframe para csv")
-    print("exit - sair do sistema")
+        except:
+            print("ERRO!")
 
 menu()
